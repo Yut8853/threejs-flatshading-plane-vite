be14123852ceb6cb3aa454395499033858168314
yk0611 <hello@junkbranding.com>
2023-02-17 09:18:29 +0900

    add-flatshading

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 import './style.css'
 import * as THREE from 'three'
+import GUI from 'lil-gui'
+
+const gui = new GUI()
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10
+  }
+}
+gui.add(world.plane, 'width', 1, 20).onChange(generatePlane);
+gui.add(world.plane, 'height', 1, 20).onChange(generatePlane)
+gui.add(world.plane, 'widthSegments', 1, 50).onChange(generatePlane);
+gui.add(world.plane, 'heightSegments', 1, 50).onChange(generatePlane);
+
+  function generatePlane() {
+    planeMesh.geometry.dispose();
+    planeMesh.geometry = new THREE.PlaneGeometry(
+      world.plane.width,
+      world.plane.height,
+      world.plane.widthSegments,
+      world.plane.heightSegments
+    );
+    const { array } = planeMesh.geometry.attributes.position;
+    for (let i = 0; i < array.length; i += 3) {
+      const x = array[i];
+      const y = array[i + 1];
+      const z = array[i + 2];
+
+      array[i + 2] = z + Math.random();
+    }
+  }
+
+
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -14,14 +49,6 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
-// const boxGeometry = new THREE.BoxGeometry(
-//   1, 1, 1
-// )
-// const material = new THREE.MeshBasicMaterial(
-//   { color: 0x00FF00 }
-// )
-// const mesh = new THREE.Mesh(boxGeometry, material)
-// scene.add(mesh)
 camera.position.z = 5
 
 const planeGeometry = new THREE.PlaneGeometry(
@@ -57,9 +84,9 @@ function animation() {
   renderer.render(scene, camera)
   // mesh.rotation.x += 0.1
   // mesh.rotation.y += 0.1
-  planeMesh.rotation.z += 0.01
-  planeMesh.rotation.x += 0.03
-  planeMesh.rotation.y += 0.01
+  // planeMesh.rotation.z += 0.01
+  // planeMesh.rotation.x += 0.03
+  // planeMesh.rotation.y += 0.01
 }
 
 animation()
